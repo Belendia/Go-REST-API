d46fd53a2ddcd0602f051c8d882e65930b0b7622
Belendia <belendia@gmail.com>
2024-10-01 11:21:26 +0300

    Add home route handler

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Go!")
+}
 
 func main() {
-	fmt.Println("Hello, Go!")
+	http.HandleFunc("/", helloWorld)
+	fmt.Println("Listening on port 9003")
+	log.Fatal(http.ListenAndServe(":9003", nil))
 }
